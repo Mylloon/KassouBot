55774937823bc9b21f727787ef93c088f7f7b1a0
Mylloon <kennel.anri@outlook.com>
2021-06-03 10:54:03 +0200

    adding custom values to request method

--- src/utils/db.py
+++ src/utils/db.py
@@ -2,7 +2,8 @@ import sqlite3
 
 class Database:
     def __init__(self):
-        self.curseur = self.createConnection("src/db/bot.sqlite3").cursor()
+        self.connexion = self.createConnection(r"src/db/bot.sqlite3")
+        self.curseur = self.connexion.cursor()
 
     def createConnection(self, path):
         """Connexion à une base de donnée SQLite"""
@@ -11,7 +12,6 @@ class Database:
         connnexion = None
         try:
             connnexion = sqlite3.connect(path)
-            print(f"Database connected with SQLite v{sqlite3.version}")
         except sqlite3.Error as e:
             print(e)
         return connnexion
@@ -25,9 +25,14 @@ class Database:
         else:
             return True
 
-    def requete(self, requete):
-        """Reqête vers la base de données"""
+    def requete(self, requete, valeurs = None):
+        """Envois une requête vers la base de données"""
         try:
-            self.curseur.execute(requete)
+            if valeurs:
+               self.curseur.execute(requete, valeurs) 
+            else:
+                self.curseur.execute(requete)
+            self.connexion.commit()
+            return self.curseur.lastrowid
         except sqlite3.Error as e:
             print(e)
