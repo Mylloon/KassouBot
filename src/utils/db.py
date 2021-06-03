3bb98edef4c7cb95bbd8a145ae5aedbd1d1c4920
Mylloon <kennel.anri@outlook.com>
2021-06-03 08:30:17 +0200

    creating db if not existing

--- src/utils/db.py
+++ src/utils/db.py
@@ -1,3 +1,26 @@
 import sqlite3
+from pathlib import Path
 
-con = sqlite3.connect('../db/bot.sqlite3')
+class Database():
+
+    def __init__(self):
+        path = Path("src/db/bot.sqlite3").absolute()
+        if not self.isDatabaseExists(path):
+            self.createDB(path)
+        self.con = sqlite3.connect(path)
+        print("DB Open")
+
+    def isDatabaseExists(self, path):
+        try:
+            Path(path).resolve(strict = True)
+        except FileNotFoundError:
+            return False
+        else:
+            return True
+
+    def createDB(self, path):
+        print("Creating DB...")
+        open(path, "x")
+
+
+Database()
