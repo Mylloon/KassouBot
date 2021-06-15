2d21d9276ba020e45bb1fb006e82eb327cedba9b
Mylloon <kennel.anri@outlook.com>
2021-06-15 09:10:34 +0200

    better replace

--- src/utils/time.py
+++ src/utils/time.py
@@ -56,7 +56,7 @@ def timestampScreen(timestamp):
 
 def timedeltaToString(time):
     """Différence entre une heure en seconde et maintenant"""
-    age = str(timedelta(seconds = time)).replace('days, ', 'jours, :').replace('day, ', 'jour, :').split(':')
+    age = str(timedelta(seconds = time)).replace('day', 'jour').replace(', ', ', :').split(':')
     if len(age) == 4:
         a = [1, 1, 1, 1] # a pour affichage
     if len(age) == 3:
