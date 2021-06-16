a71cc40ac1e4119a7f99a7a4fd19b1331d9e6e5d
Mylloon <kennel.anri@outlook.com>
2021-06-16 13:59:32 +0200

    ajout subreddit

--- src/cogs/internet.py
+++ src/cogs/internet.py
@@ -34,7 +34,7 @@ class Internet(commands.Cog):
             subredditchoix = choice(['memes', 'goodanimemes', 'dankmemes', 'DeepFried',
             'educationalmemes', 'funny', 'marvelmemes', 'me_irl', 'meme', 'MemeEconomy', 'Memes_Of_The_Dank', 'MinecraftMemes',
             'physicsmemes', 'blackpeopletwitter', 'metal_me_irl', '195', 'shittyadviceanimals', 'meirl',
-            '2meirl4meirl', 'AdviceAnimals', 'weirdmemes'])
+            '2meirl4meirl', 'AdviceAnimals', 'weirdmemes', 'LeagueOfMemes'])
 
         try:
             async with Reddit(client_id = environ['TOKEN_REDDIT_CLIENT_ID'], client_secret = environ['TOKEN_REDDIT_CLIENT_SECRET'], user_agent = f"disreddit /u/{environ['TOKEN_REDDIT_USER_AGENT']}, http://localhost:8080") as reddit:
